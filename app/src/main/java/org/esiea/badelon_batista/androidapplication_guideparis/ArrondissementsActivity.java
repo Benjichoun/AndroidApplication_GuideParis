b56943cd497cdaffe7dcddba967c6f79abbd5ba8
Benjichoun <batista@et.esiea.fr>
2015-12-28 17:05:35 +0100

    Modification de l'activity "Arrondissements" avec l'ajout d'un Material View Ajout d'un menu de navigation

---
+++ app/src/main/java/org/esiea/badelon_batista/androidapplication_guideparis/ArrondissementsActivity.java
@@ -0,0 +1,299 @@
+package org.esiea.badelon_batista.androidapplication_guideparis;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.github.florent37.materialviewpager.MaterialViewPager;
+
+
+public class ArrondissementsActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    Intent intent; //Intent pour changer d'activity
+    MaterialViewPager materialViewPager; //Init de la materialview sur notre activity
+    View headerLogo;//Init de l'en-tête de la materialView
+    ImageView headerLogoContent;//Init du logo sur l'en-tête
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arrondissements);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Action du bouton "mail/contact" à supp?
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        //Bar de navigation
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        //Création du content du "Material View"
+
+        final int tabCount = 8; //Nombre d'onglet
+
+        //les vues définies dans @layout/header_logo
+        headerLogo = findViewById(R.id.headerLogo);
+        headerLogoContent = (ImageView) findViewById(R.id.headerLogoContent);
+
+        //le MaterialViewPager
+        this.materialViewPager = (MaterialViewPager) findViewById(R.id.materialViewPager);
+
+        //remplir le ViewPager
+        this.materialViewPager.getViewPager().setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+
+            @Override
+            public Fragment getItem(int position) {
+                //je créé pour chaque onglet un RecyclerViewFragment
+                return RecyclerViewFragment.newInstance();
+            }
+
+            @Override
+            public int getCount() {
+                return tabCount;
+            }
+
+            //le titre à afficher pour chaque page
+            @Override
+            public CharSequence getPageTitle(int position) {
+                switch (position) {
+
+                    case 0:
+                        return getResources().getString(R.string.arrondissements1);
+                    case 1:
+                        return getResources().getString(R.string.arrondissements2);
+                    case 2:
+                        return getResources().getString(R.string.arrondissements3);
+                    case 3:
+                        return getResources().getString(R.string.arrondissements4);
+                    case 4:
+                        return getResources().getString(R.string.arrondissements5);
+                    case 5:
+                        return getResources().getString(R.string.arrondissements6);
+                    case 6:
+                        return getResources().getString(R.string.arrondissements7);
+                    case 7:
+                        return getResources().getString(R.string.arrondissements8);
+                    default:
+                        return "Page " + position;
+                }
+            }
+
+
+
+        int oldItemPosition = -1;
+        @Override
+        public void setPrimaryItem(ViewGroup container, int position, Object object) {
+            super.setPrimaryItem(container, position, object);
+
+            //seulement si la page est différente
+            if (oldItemPosition != position) {
+                oldItemPosition = position;
+
+                //définir la nouvelle couleur et les nouvelles images
+                String imageUrl = null;
+                int color = Color.BLACK;
+                Drawable newDrawable = null;
+
+                switch (position) {
+                    case 0:
+                        imageUrl = "http://www.paris-en-photos.fr/wp-content/uploads/2008/07/statue-jardin-les-halles-150x150.png";
+                        color = getResources().getColor(R.color.blue);
+                        newDrawable = getResources().getDrawable(R.drawable.ic_menu_arrond);
+                        break;
+                    case 1:
+                       imageUrl = "http://www.paris-en-photos.fr/wp-content/uploads/2008/07/passage-grand-cerf-150x150.png";
+                        color = getResources().getColor(R.color.orange);
+                        newDrawable = getResources().getDrawable(R.drawable.ic_menu_arrond);
+                        break;
+                    case 2:
+                        imageUrl = "http://www.paris-en-photos.fr/wp-content/uploads/2008/10/piazza-beaucourg-156-saint-martin-150x150.png";
+                        color = getResources().getColor(R.color.cyan);
+                        newDrawable = getResources().getDrawable(R.drawable.ic_menu_arrond);
+                        break;
+                    case 3:
+                        imageUrl = "http://www.paris-en-photos.fr/wp-content/uploads/2008/07/elephant-fontaine-niki-st-phalle-150x150.png";
+                        color = getResources().getColor(R.color.green);
+                        newDrawable = getResources().getDrawable(R.drawable.ic_menu_arrond);
+                        break;
+                    case 4:
+                        imageUrl = "http://www.paris-en-photos.fr/wp-content/uploads/2008/07/elephant-fontaine-niki-st-phalle-150x150.png";
+                        color = getResources().getColor(R.color.green);
+                        newDrawable = getResources().getDrawable(R.drawable.ic_menu_arrond);
+                        break;
+                    case 5:
+                        imageUrl = "http://www.paris-en-photos.fr/wp-content/uploads/2008/07/elephant-fontaine-niki-st-phalle-150x150.png";
+                        color = getResources().getColor(R.color.green);
+                        newDrawable = getResources().getDrawable(R.drawable.ic_menu_arrond);
+                        break;
+                    case 6:
+                        imageUrl = "http://www.paris-en-photos.fr/wp-content/uploads/2008/07/elephant-fontaine-niki-st-phalle-150x150.png";
+                        color = getResources().getColor(R.color.green);
+                        newDrawable = getResources().getDrawable(R.drawable.ic_menu_arrond);
+                        break;
+                    case 7:
+                        imageUrl = "http://www.paris-en-photos.fr/wp-content/uploads/2008/07/elephant-fontaine-niki-st-phalle-150x150.png";
+                        color = getResources().getColor(R.color.green);
+                        newDrawable = getResources().getDrawable(R.drawable.ic_menu_arrond);
+                        break;
+                    case 8:
+                        imageUrl = "http://www.paris-en-photos.fr/wp-content/uploads/2008/07/elephant-fontaine-niki-st-phalle-150x150.png";
+                        color = getResources().getColor(R.color.green);
+                        newDrawable = getResources().getDrawable(R.drawable.ic_menu_arrond);
+                        break;
+                }
+
+                //puis modifier les images/couleurs
+                int fadeDuration = 400;
+                materialViewPager.setColor(color, fadeDuration);
+                materialViewPager.setImageUrl(imageUrl, fadeDuration);
+                toggleLogo(newDrawable,color,fadeDuration);
+
+            }
+        }
+    });
+
+    //relie les tabs au viewpager
+    this.materialViewPager.getPagerTitleStrip().setViewPager(this.materialViewPager.getViewPager());
+}
+
+    private void toggleLogo(final Drawable newLogo, final int newColor, int duration){
+
+        //animation de disparition
+        final AnimatorSet animatorSetDisappear = new AnimatorSet();
+        animatorSetDisappear.setDuration(duration);
+        animatorSetDisappear.playTogether(
+                ObjectAnimator.ofFloat(headerLogo, "scaleX", 0),
+                ObjectAnimator.ofFloat(headerLogo, "scaleY", 0)
+        );
+
+        //animation d'apparition
+        final AnimatorSet animatorSetAppear = new AnimatorSet();
+        animatorSetAppear.setDuration(duration);
+        animatorSetAppear.playTogether(
+                ObjectAnimator.ofFloat(headerLogo, "scaleX", 1),
+                ObjectAnimator.ofFloat(headerLogo, "scaleY", 1)
+        );
+
+        //après la disparition
+        animatorSetDisappear.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+
+                //modifie la couleur du cercle
+                ((GradientDrawable) headerLogo.getBackground()).setColor(newColor);
+
+                //modifie l'image contenue dans le cercle
+                headerLogoContent.setImageDrawable(newLogo);
+
+                //démarre l'animation d'apparition
+                animatorSetAppear.start();
+            }
+        });
+
+        //démarre l'animation de disparition
+        animatorSetDisappear.start();
+}
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Menu de navigation selon la selection de l'item nous allons sur l'activity
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camara) {
+            // Handle the camera action
+            intent= new Intent(this,MainActivity.class);
+            startActivity(intent);
+
+        } else if (id == R.id.nav_gallery) {
+            intent= new Intent(this,ArrondissementsActivity.class);
+            startActivity(intent);
+
+        } else if (id == R.id.nav_slideshow) {
+            intent= new Intent(this,CalendrierActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+}
